9d6d67ec5ceb4f7880a37591bd7de65e5bf55ef5
MA7 <amini141@gmail.com>
2013-07-10 14:12:00 +0430

    initial

---
+++ ap-Chehrak.php
@@ -0,0 +1,87 @@
+<?php
+/*
+	Plugin Name:	AP Chehrak
+	Plugin URI:		http://chehrak.com
+	Description:	A simple plugin that adds the Chehrak photo associated with the user's email to their profile page. If they do not have a chehrak account, it displays a link to create one. MultiSite compatable!
+
+	Version: 		1.0
+	Author: 		Masoud Amini
+	Author URI: 	http://haftir.ir
+	License: 		GPL2
+*/
+
+/*  Copyright 2013 Masoud Amini 
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+// Add to Profile page
+	function ap_chehrak_options($user){
+?>
+			<h3>Profile Picture</h3>
+			
+			<table class="form-table">
+				<tr>
+					<th><label>Your profile picture</label></th>
+					<td width="100">
+<?php
+		
+		$source = 'http://rokh.chehrak.com/';
+		$default = '';
+		$size = '100';
+		$chehrak_query_str = '?size=' . $size;
+
+		global $profileuser;
+		$id = $profileuser->ID;
+		$email = $profileuser->user_email;
+		$name = $profileuser->display_name;
+		
+		$src = $source;
+		$src .= md5(strtolower($email));
+		$src .= $chehrak_query_str;
+
+		$avatar = "<img src='$src' class='avatar avatar-$size' alt='$name' height='$size' width='$size' />";
+		
+		echo($avatar);
+?>
+					</td>
+					<td><?php // echo "<span class='description'>$id &bull; $name &bull; $email</span><br>"; ?>
+						Your Chehrak is an image that follows you from site to site appearing<br/>
+						beside your name when you do things like comment or post on a blog.
+						<br/><br/>
+						We use Chehraks to display your profile picture. If you do not have a<br/>
+						Chehrak you can sign up at <a href="http://Chehrak.com">Chehrak.com</a> to have a picture of <br/>
+						yourself appear not only here, but on all sorts of other sites!
+					</td>
+				</tr>
+				<tr>
+					<th></th>
+					<td></td>
+					<td>
+							<a class="button" href="http://Chehrak.com/" target="_blank">Get your Chehrak today &raquo;</a>
+					</td>
+				</tr>
+			</table>
+
+<?php	
+	}
+
+
+// Hook
+add_action('show_user_profile', 'ap_Chehrak_options');
+add_action('edit_user_profile', 'ap_Chehrak_options');
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Plugin Name ===
+Contributors: M.Amini
+Plugin Name: AP Chehraks
+Tags: profile, profile picture, Chehrak, avatar, wpms, multisite
+Author URI: http://haftir.ir
+Author: Masoud Amini
+Requires at least: 2.0
+Tested up to: 3.3.1
+Stable tag: 1.0
+
+A simple plugin that adds the Chehrak photo associated with the user's email to their profile page... MultiSite compatable!
+
+== Description ==
+
+The  *Chehrak* plugin adds the Chehrak photo associated with the user's email to their **profile page**. If they do not have a Chehrak account, it displays a link to create one. And is **MultiSite compatable!**
+
+This plugin does **not** add Chehrak support to your theme.
+
+
+== Installation ==
+= Single WordPress Install = 
+1. Upload the **ap-Chehraks** folder to the **/wp-content/plugins/** directory
+1. *Activate* the plugin through the *Plugins* menu in WordPress
+
+= WordPress MultiSite Install =
+1. Upload the **ap-Chehraks** folder to the **/wp-content/plugins/** directory
+1. *Network Activate* the plugin through the *Network Admin > Plugins* menu in WordPress
+       **--OR--**
+1. *Activate* the plugin through the *Plugins* menu in WordPress on an individual site basis
+
+== Changelog ==
+
+= 1.0 =
+* First release
+
+== Frequently Asked Questions ==
+= Do I need to do anything else for AP Chehraks to work? =
+Nope, just upload and activate. There are not options, so the code'll do the rest.
+
+= What in the world is a Chehrak? =
+**A Globally Recognized Avatar** - Your Chehrak is an image that follows you from site to site appearing beside your name when you do things like comment or post on a blog. Avatars help identify your posts on blogs and web forums, so why not on any site?
+
+*From the [Chehrak.com](http://www.Chehrak.com) website*
+
+= How do I create a Chehrak? =
+To create your free Chehrak, visit [Chehrak.com](http://www.Chehrak.com)
+
+
+== Screenshots ==
+1. Profile Picture added to "Your Profile" options page
